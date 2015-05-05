7178f033f751b7e4fc12c31a210e074f31971853
Kunal Mehta <legoktm@gmail.com>
2015-05-04 18:48:06 -0700

    test: Automatically get list of i18n directories from extension.json
    Change-Id: I6394b72c0b21dc51bf1816916157961e3e2fd48d

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,10 +7,9 @@
 /*jshint node:true */
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	var conf = grunt.file.readJSON( 'extension.json' );
 	grunt.initConfig( {
-		banana: {
-			all: ["i18n/"]
-		}
+		banana: conf.MessagesDirs
 	} );
 
 	grunt.registerTask( 'test', [ 'banana' ] );
