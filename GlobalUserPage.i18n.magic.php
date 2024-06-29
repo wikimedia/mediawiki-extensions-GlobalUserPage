b8d8949a292f282027105ee8ddd10ed8ef0aedfd
xtex <xtexchooser@duck.com>
2024-06-29 17:52:31 +0800

    Add Simplified Chinese magic word alias
    Change-Id: I8a6908ad024fdcf9676573dca3cfc000c74ecd49
    (cherry picked from commit ed8d0748aa7596d59fd86117d565256254ffc95b)

--- GlobalUserPage.i18n.magic.php
+++ GlobalUserPage.i18n.magic.php
@@ -2,6 +2,12 @@
 
 $magicWords = [];
 
+/** English (English) */
 $magicWords['en'] = [
 	'noglobal' => [ 1, '__NOGLOBAL__' ],
+];
+
+/** Simplified Chinese (中文（简体）) */
+$magicWords['zh-hans'] = [
+	'noglobal' => [ 1, '__禁用全域用户页__' ],
 ];
